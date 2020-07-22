4448be4124305050c7ef9517e83f78bd40441fd1
Ryan-Newell <55711643+Ryan-Newell@users.noreply.github.com>
2020-07-22 13:08:45 -0400

    demo working image selection

--- app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/MainActivity.java
+++ app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/MainActivity.java
@@ -1,7 +1,15 @@
 package com.cs5520.numad20su_congressmobile.controllers;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,6 +19,8 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import com.cs5520.numad20su_congressmobile.R;
 import com.cs5520.numad20su_congressmobile.databinding.ActivityMainBinding;
 
+import java.io.FileNotFoundException;
+
 // TODO For each tab, grab content for both House and Senate
 // TODO Respond to clicks of actions in action bar
 // TODO Put in a working search bar
@@ -19,6 +29,7 @@ import com.cs5520.numad20su_congressmobile.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    private ImageView targetImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        targetImage = (ImageView)findViewById(R.id.test);
 
         // Create the adapter that will return a fragment for each section
         FragmentPagerAdapter mPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager(),
@@ -69,8 +81,56 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
         return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.manage_profile:
+                //Toast.makeText(this, "braff", Toast.LENGTH_LONG).show();
+                getImage(this.getCurrentFocus());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void getImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 0);
+        //Toast.makeText(this, "braff", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            Uri targetUri = data.getData();
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                targetImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+//    public void getImage(View arg0) {
+//        // TODO Auto-generated method stub
+////        Intent intent = new Intent(Intent.ACTION_PICK,
+////                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+////        startActivityForResult(intent, 0);
+//        Toast.makeText(this, "braff", Toast.LENGTH_LONG).show();
+//
+//
+//    }
 
 
 }
