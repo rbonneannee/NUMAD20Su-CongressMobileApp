3dc2532ff6acef43b3cc5cea4eec3a7b02fc02e8
Rebecca Bonne-Annee <rebecca.m.bonneannee@gmail.com>
2020-08-01 13:44:41 -0400

    Inflate non-recyclerview layout; add scrollListener for future purposes

--- app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/CommitteesFragment.java
+++ app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/CommitteesFragment.java
@@ -2,21 +2,28 @@ package com.cs5520.numad20su_congressmobile.controllers;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.cs5520.numad20su_congressmobile.R;
 import com.cs5520.numad20su_congressmobile.content.CommitteesViewContent;
+import com.google.android.material.textfield.TextInputEditText;
 
 /**
  * A fragment representing a list of Items.
  */
 public class CommitteesFragment extends Fragment {
+
+    private boolean isLoading = false;
+    private CommitteesViewContent committeesViewContent;
+    private TextInputEditText searchFld;
 
     public CommitteesFragment() {
     }
@@ -31,16 +38,54 @@ public class CommitteesFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_committees, container, false);
 
-        CommitteesViewContent committeesViewContent = new CommitteesViewContent(this.getContext());
-        committeesViewContent.getBills();
+        this.committeesViewContent = new CommitteesViewContent(this.getContext());
+        this.committeesViewContent.getCommittees();
 
         // Set the adapter
-        if (view instanceof RecyclerView) {
-            Context context = view.getContext();
-            RecyclerView recyclerView = (RecyclerView) view;
-            recyclerView.setLayoutManager(new LinearLayoutManager(context));
-            recyclerView.setAdapter(committeesViewContent.getViewAdapter());
-        }
-        return view;
+        RecyclerView recyclerView = view.findViewById(R.id.list_committees);
+        Context context = recyclerView.getContext();
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(committeesViewContent.getViewAdapter());
+        initScrollListener(recyclerView);
+
+        this.searchFld = view.findViewById(R.id.textInputEditText_committeeName);
+        view.findViewById(R.id.imageButton_searchCommittee)
+                .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String committeeName = searchFld.getText().toString();
+                // TODO implement and call billsViewContent.searchCommittee(committeeName);
+            }
+        });
+
+        return  view;
+    }
+
+    private void initScrollListener(RecyclerView recyclerView) {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView
+                        .getLayoutManager();
+
+                if (!isLoading) {
+                    if (linearLayoutManager != null
+                        && (linearLayoutManager.findLastCompletelyVisibleItemPosition()
+                            == committeesViewContent.getResultList().size() - 1)) {
+
+                        // Launch data load in a new thread
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                committeesViewContent.loadMore();
+                                isLoading = false;
+                            }
+                        }, 5);
+                    }
+                }
+            }
+        });
     }
 }
