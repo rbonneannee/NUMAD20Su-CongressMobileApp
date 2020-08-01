8cb14e9b9641e23b05e21f02d71b1ed3d96f3ce8
Rebecca Bonne-Annee <rebecca.m.bonneannee@gmail.com>
2020-08-01 17:21:28 -0400

    Refactor conditionalReset and rename methods and fields

--- app/src/main/java/com/cs5520/numad20su_congressmobile/content/services/BillsViewContent.java
+++ app/src/main/java/com/cs5520/numad20su_congressmobile/content/services/BillsViewContent.java
@@ -10,54 +10,69 @@ import com.cs5520.numad20su_congressmobile.layoutAdapters.BillsRecyclerViewAdapt
 
 import java.util.List;
 
-// TODO Gather bills from both chambers for each endpoint
 // TODO Create filter methods to be called from a filter view
+/**
+ * This class is a service that provides bill information from the ProPublica Congress database to
+ * the application. BillViewContent objects have endpoints to which they can make GET requests,
+ */
 public class BillsViewContent extends AbstractViewContent<Bill> {
 
+    // Classifies and holds type of information requested from server
     enum RequestEnum {RECENT, SUBJECT, KEYWORD;}
+    private RequestEnum prevRequestEnum;
+    private String prevKeywordQuery;
+    // TODO determine if application will support search by subject
+    private String prevSubjectQuery;
 
-    private static final String ENDPOINT = "https://api.propublica.org/congress/v1/116/both/bills/active.json";
-    private static final String ENDPOINT_SUBJECT_SEARCH = "https://api.propublica.org/congress/v1/bills/subjects/";
-    private static final String ENDPOINT_KEYWORD_SEARCH = "https://api.propublica.org/congress/v1/bills/search.json?query=";
-    private static final int OFFSET_INCREMENT = 20;
-
-    private static int offset = 0;
-
-    private String searchQuery;
-    private String subjectQuery;
-    private RequestEnum lastRequestType;
+    // Supported server endpoints
+    private final String endpointBillsRecent;
+    private final String endpointBillsSubjectSearch;
+    private final String endpointBillsKeywordSearch;
 
     public BillsViewContent(Context context) {
         super(context);
         this.viewAdapter = new BillsRecyclerViewAdapter(this.resultList);
+        this.prevRequestEnum = RequestEnum.RECENT;
+        this.prevKeywordQuery  = "";
+
+        // Endpoint URLs
+        this.endpointBillsRecent =
+                "https://api.propublica.org/congress/v1/116/both/bills/active.json?offset=";
+        this.endpointBillsSubjectSearch =
+                "https://api.propublica.org/congress/v1/bills/subjects/";
+        this.endpointBillsKeywordSearch =
+                "https://api.propublica.org/congress/v1/bills/search.json?query=";
     }
 
     public void getRecentBills() {
-        conditionalReset("", "", RequestEnum.RECENT, lastRequestType);
-        this.lastRequestType = RequestEnum.RECENT;
-        this.submitRequest(ENDPOINT + "?offset=" + offset);
-        incrementOffset();
+        if (!conditionalReset(RequestEnum.RECENT, this.DEFAULT_QUERY, this.DEFAULT_QUERY)){
+            incrementOffset();
+        }
+        this.submitRequest(this.endpointBillsRecent + this.offset);
     }
 
-    public void getRecentBillsBySubject(String subject) {
-        this.lastRequestType = RequestEnum.SUBJECT;
-        conditionalReset(subject, this.subjectQuery, RequestEnum.SUBJECT, lastRequestType);
-        this.subjectQuery = subject;
-        this.submitRequest(ENDPOINT_SUBJECT_SEARCH + subject + ".json" + "?offset=" + offset);
-        incrementOffset();
+    /* TODO determine if application will support search by subject
+    public void getBillsWithSubject(String subject) {
+        this.prevRequestType = RequestEnum.SUBJECT;
+        conditionalReset(subject, this.prevSubjectQuery, RequestEnum.SUBJECT, prevRequestType);
+        this.prevSubjectQuery = subject;
+        this.submitRequest(endpointBillsSubjectSearch + subject + ".json" + "?offset=" + offset);
+        this.incrementOffset();
     }
+     */
 
-    public void searchBills(String query) {
-        this.lastRequestType = RequestEnum.KEYWORD;
-        conditionalReset(query, this.searchQuery, RequestEnum.KEYWORD, lastRequestType);
-        this.searchQuery = query;
-        this.submitRequest(ENDPOINT_KEYWORD_SEARCH + query + "&offset=" + offset);
-        incrementOffset();
+    public void getBillsWithKeyword(String keyword) {
+        if (conditionalReset(RequestEnum.KEYWORD, keyword, this.prevKeywordQuery)) {
+            this.prevKeywordQuery = keyword;
+        } else {
+            incrementOffset();
+        }
+        this.submitRequest(endpointBillsKeywordSearch + keyword + "&offset=" + this.offset);
     }
 
     @Override
     List<Bill> getListFromJsonText(String jsonText) {
-        switch (this.lastRequestType) {
+        switch (this.prevRequestEnum) {
             case RECENT:
             case KEYWORD:
                 return BillsJsonTextHandler.extract(jsonText);
@@ -68,33 +83,32 @@ public class BillsViewContent extends AbstractViewContent<Bill> {
         }
     }
 
-    private void incrementOffset() {
-        offset += OFFSET_INCREMENT;
-    }
+
 
     // Reset when either the query changes or the request type changes
-    private boolean conditionalReset(String s1,
-                                     String s2,
-                                     RequestEnum r1,
-                                     RequestEnum r2) {
-        boolean result = false;
-        if ((!s1.equals(s2)) || (r1 != r2)) {
-            offset = 0;
+    private boolean conditionalReset(RequestEnum currentRequestEnum, String currentQuery,
+                                     String prevQuery) {
+        boolean isReset = false;
+        if ((!currentQuery.equals(prevQuery)) || (currentRequestEnum != this.prevRequestEnum)) {
+            this.offset = 0;
             this.resultList.clear();
+            this.prevRequestEnum = currentRequestEnum;
+            isReset = true;
         }
-        return result;
+        return isReset;
     }
 
     public void loadMore() {
-        switch (this.lastRequestType) {
+        switch (this.prevRequestEnum) {
             case RECENT:
                 getRecentBills();
                 break;
             case KEYWORD:
-                searchBills(this.searchQuery);
+                getBillsWithKeyword(this.prevKeywordQuery);
                 break;
             case SUBJECT:
-                getRecentBillsBySubject(this.subjectQuery);
+                // TODO determine if application will support search by subject
+                // getBillsWithSubject(this.prevSubjectQuery);
                 break;
         }
     }
