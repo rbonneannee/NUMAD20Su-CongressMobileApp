64c2f965a812b3b4e54872cb165c0e4d5a25ad42
Ryan-Newell <55711643+Ryan-Newell@users.noreply.github.com>
2020-08-06 19:44:17 -0400

    bill details

--- app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/BillDetailsActivity.java
+++ app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/BillDetailsActivity.java
@@ -3,7 +3,9 @@ package com.cs5520.numad20su_congressmobile.controllers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.cs5520.numad20su_congressmobile.R;
@@ -17,7 +19,8 @@ public class BillDetailsActivity extends AppCompatActivity {
     private TextView billIntroduced;
     private TextView billSponsor;
     private TextView billCommittees;
-
+    private TextView testText;
+    private String urlText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +31,7 @@ public class BillDetailsActivity extends AppCompatActivity {
         billIntroduced = findViewById(R.id.bill_introduced);
         billSponsor = findViewById(R.id.bill_sponsor);
         billCommittees = findViewById(R.id.bill_committees);
-
+        testText = findViewById(R.id.bill_status);
 
         Intent openDetailsIntent = getIntent();
 
@@ -41,6 +44,8 @@ public class BillDetailsActivity extends AppCompatActivity {
         //billSponsor.setText(bill.getSponsor_name());
         sponsorSetup();
         billCommittees.setText(bill.getCommittees());
+        urlText = bill.getCongressdotgov_url() + "/text";
+        //testText.setText(bill.getCongressdotgov_url());
     }
 
 
@@ -49,5 +54,12 @@ public class BillDetailsActivity extends AppCompatActivity {
                 bill.getSponsor_party() + ", " + bill.getSponsor_state() + ")";
         billSponsor.setText(sponsorText);
 
+    }
+
+    public void goToPDF (View v) {
+        //Uri uriUrl = Uri.parse("https://www.congress.gov/bill/114th-congress/house-bill/1314/text");
+        Uri uriUrl = Uri.parse(urlText);
+        Intent launchFullText = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(launchFullText);
     }
 }
