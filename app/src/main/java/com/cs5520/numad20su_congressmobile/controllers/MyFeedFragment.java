7eb91749cc29a4d623a4872f4b0632d77d073447
matthiasdenu <matthiasdenu@icloud.com>
2020-08-07 16:36:43 -0400

    Populate MyFeed with followed bills

--- app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/MyFeedFragment.java
+++ app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/MyFeedFragment.java
@@ -1,6 +1,5 @@
 package com.cs5520.numad20su_congressmobile.controllers;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +8,8 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.cs5520.numad20su_congressmobile.R;
-import com.cs5520.numad20su_congressmobile.content.models.DummyContent;
-import com.cs5520.numad20su_congressmobile.layoutAdapters.MyFeedRecyclerViewAdapter;
+import com.cs5520.numad20su_congressmobile.content.services.MyFeedBillsContent;
+import com.cs5520.numad20su_congressmobile.controllers.FollowInterface.TYPE;
 
 // TODO Put logic to populate 'My Feed' here
 
@@ -30,16 +29,22 @@ public class MyFeedFragment extends Fragment implements FollowTrigger {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.fragment_my_feed, container, false);
+    RecyclerView billsView = view.findViewById(R.id.my_feed_bills_list);
+    RecyclerView committeesView = view.findViewById(R.id.my_feed_committees_list);
+    RecyclerView membersView = view.findViewById(R.id.my_feed_members_list);
 
-    // Set the adapter
-    if (view instanceof RecyclerView) {
-      Context context = view.getContext();
-      RecyclerView recyclerView = (RecyclerView) view;
-      recyclerView.setLayoutManager(new LinearLayoutManager(context));
+    // Set the layout managers
+    billsView.setLayoutManager(new LinearLayoutManager(billsView.getContext()));
+    committeesView.setLayoutManager(new LinearLayoutManager(committeesView.getContext()));
+    membersView.setLayoutManager(new LinearLayoutManager(membersView.getContext()));
 
-      // TODO Replace this adapter
-      recyclerView.setAdapter(new MyFeedRecyclerViewAdapter(DummyContent.ITEMS));
-    }
+    // Create the content providers
+    MyFeedBillsContent myFeedBillsContent =
+        new MyFeedBillsContent(callback.following(TYPE.Bill), this.getContext(), callback);
+
+    // Set the adapters
+    billsView.setAdapter(myFeedBillsContent.getAdapter());
+
     return view;
   }
 
