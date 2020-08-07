7ab28d626563ce13fe07b6bd65b44a31eb19b83a
Rebecca Bonne-Annee <rebecca.m.bonneannee@gmail.com>
2020-08-07 17:17:31 -0400

    Move to 'enums' folder

---
+++ app/src/main/java/com/cs5520/numad20su_congressmobile/content/enums/ChamberType.java
@@ -0,0 +1,4 @@
+package com.cs5520.numad20su_congressmobile.content.enums;
+
+public class ChamberType {
+}
--- app/src/main/java/com/cs5520/numad20su_congressmobile/content/enums/GetRequestType.java
+++ app/src/main/java/com/cs5520/numad20su_congressmobile/content/enums/GetRequestType.java
@@ -1,4 +1,4 @@
-package com.cs5520.numad20su_congressmobile.content.services;
+package com.cs5520.numad20su_congressmobile.content.enums;
 
 public enum GetRequestType {
     ALL, TEXT_SEARCH, SUBJECT_SEARCH, FILTER
