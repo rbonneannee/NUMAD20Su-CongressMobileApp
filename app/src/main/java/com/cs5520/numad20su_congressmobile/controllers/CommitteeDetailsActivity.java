aa8a91f80d11a206518e496c010025a1b6b5a03c
Ryan-Newell <55711643+Ryan-Newell@users.noreply.github.com>
2020-08-07 18:18:50 -0400

    committee and member details'

--- app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/CommitteeDetailsActivity.java
+++ app/src/main/java/com/cs5520/numad20su_congressmobile/controllers/CommitteeDetailsActivity.java
@@ -2,15 +2,49 @@ package com.cs5520.numad20su_congressmobile.controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.cs5520.numad20su_congressmobile.R;
+import com.cs5520.numad20su_congressmobile.content.models.Committee;
 
 public class CommitteeDetailsActivity extends AppCompatActivity {
+
+    private Committee committee;
+    private TextView title;
+    private TextView chamber;
+    private TextView chair;
+    //private TextView
+    //private TextView
+    //private TextView
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_committee_details);
+        title = findViewById(R.id.committee_title);
+        chamber = findViewById(R.id.committee_chamber);
+        chair = findViewById(R.id.committee_chair);
+
+
+        Intent openDetailsIntent = getIntent();
+
+        committee = openDetailsIntent.getParcelableExtra("committee");
+        title.setText(committee.name);
+        chamber.setText(committee.chamber);
+        //chair.setText(committee.chair +);
+        chairSetup();
     }
+
+    //TODO onclick to look at person details
+    private void chairSetup() {
+        String chairText = committee.chair + " (" +
+                committee.chair_party + ", " + committee.chair_state + ")";
+        chair.setText(chairText);
+
+    }
+
+    //TODO recyclerview subcommittee? IDK maybe too ambitious
 }
